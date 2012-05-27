17e022f6bd13be833813d27425da1d605644da1f
Jeromy Stroh <jstroh@pixelatedgames.com>
2012-05-26 23:41:41 -0700

    pcompile.bat is what I'm using to create the code files from the proto file fantasy_messages.proto. It's a mess of files that include a user created as3 generator and Google created java generator for now.

---
+++ proto/pcompile.bat
@@ -0,0 +1,3 @@
+protoc -I="." --java_out="..\fos\src" "fantasy_messages.proto"
+
+REM protoc --plugin=protoc-gen-as3="protoc-gen-as3.bat" --as3_out="C:\Users\J\Adobe Flash Builder 4.6\Fantasy\src" "fantasy_messages.proto"
---
+++ proto/protoc-gen-as3.bat
@@ -0,0 +1,2 @@
+@cd %~dp0
+@java -jar protoc-gen-as3.jar
