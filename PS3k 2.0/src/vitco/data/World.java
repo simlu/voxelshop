2d0c7abe607acfb35ea841e18bd477162a0cb7f9
A Z <Amy.Zendel@googlemail.com>
2012-07-15 00:26:18 +0200

    Basic PS3k 2.0 frame. Nothing to see here.

---
+++ PS3k 2.0/src/vitco/data/World.java
@@ -0,0 +1,11 @@
+package vitco.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: VM Win 7
+ * Date: 7/14/12
+ * Time: 10:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class World implements WorldInterface {
+}
