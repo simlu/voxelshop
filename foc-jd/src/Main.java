c46d88f261243fe81dda0ef082a49ffa4233bc07
Jeromy Stroh <jstroh@pixelatedgames.com>
2012-07-15 05:12:38 -0700

    Java Client shell using LWGL

--- foc-jd/src/Main.java
+++ foc-jd/src/Main.java
@@ -1,14 +1,7 @@
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-
 public class Main {
     public Main(){
-        try {
-            Display.setDisplayMode(new DisplayMode(800, 600));
-        } catch (LWJGLException e) {
-            e.printStackTrace();
-        }
+        Window window = new Window();
+        window.start();
     }
 
     public static void main(String[] args){
