e821d9503e9ccd434efb9328ee93d3f01dc7b891
blackflux <blackflux@unfine.de>
2014-02-18 20:31:20 +0100

    - fixed kvx importer (color values were read incorrectly)

--- PS4k/src/com/vitco/importer/KvxImporter.java
+++ PS4k/src/com/vitco/importer/KvxImporter.java
@@ -76,9 +76,9 @@ public class KvxImporter extends AbstractImporter {
         raf.seek(raf.length() - 768);
         int[] colPalette = new int[256];
         for (int i = 0; i < 256; i++) {
-            int r = raf.read();
-            int g = raf.read();
-            int b = raf.read();
+            int r = Math.min(255,Math.max(0,Math.round((raf.read() * 255)/63f)));
+            int g = Math.min(255,Math.max(0,Math.round((raf.read() * 255)/63f)));
+            int b = Math.min(255,Math.max(0,Math.round((raf.read() * 255)/63f)));
             colPalette[i] = new Color(r, g, b).getRGB();
         }
 
