142e4fc03993deabc9b681aa00036ce8caef2434
blackflux <blackflux@unfine.de>
2014-07-27 17:23:01 -0700

    .gitignore is now working

--- PS4k/out/artifacts/Readme.txt
+++
@@ -1,8 +0,0 @@
-- "publish.bat" contains absolute filepath to WinSCP
-
-- To add new Jars:
-Add them to the lib folder
--> if this is a new subdir, add that to dependencies (modules)
--> and add to the artifact build "lib" (jar, so they are exported)
-Add to classpath (PS3k.jar)
-Add to GetDown getdown.txt
--- PS4k/out/artifacts/publish.bat
+++
@@ -1,8 +0,0 @@
-del "%CD%\webdata\main.jar"
-copy "%CD%\PS4k_jar\main.jar" "%CD%\webdata\main.jar"
-del "%CD%\webdata\images.jar"
-copy "%CD%\PS4k_jar\images.jar" "%CD%\webdata\images.jar"
-rmdir /s /q "%CD%\webdata\lib" 
-xcopy /i "%CD%\PS4k_jar\lib" "%CD%\webdata\lib"
-java -classpath "%CD%\webdata\getdown-client-new.jar" com.threerings.getdown.tools.Digester "%CD%\webdata\
-"C:\Users\flux\Dropbox\tools\WinSCP\WinSCP.exe" /console /script=upload-script.txt
