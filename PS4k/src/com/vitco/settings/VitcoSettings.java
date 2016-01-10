4f8eb6eca9c13f15023e30468a406492c5e20f9e
blackflux <blackflux@unfine.de>
2016-01-10 12:29:04 -0800

    - updated version number

--- PS4k/src/com/vitco/settings/VitcoSettings.java
+++ PS4k/src/com/vitco/settings/VitcoSettings.java
@@ -49,7 +49,7 @@ public final class VitcoSettings {
     public static final Color TEXTURE_BORDER_ACTIVE = Color.ORANGE;
     public static final Color TEXTURE_BORDER_SELECTED = Color.RED;
 
-    public static final String VERSION_ID = "1.4.46";
+    public static final String VERSION_ID = "1.5.01";
     // version id
     public static final String TITLE_STRING = "VoxelShop - Alpha Version (V" + VERSION_ID + ")";
 
