6dd21881576db0ff96c9de7935d344e63091c6a7
blackflux <blackflux@unfine.de>
2016-10-30 14:19:49 -0700

    #68 Simplified and improved (check state of hidden check boxes is now stored/restored correctly, check state is no longer touched)

--- PS4k/src/com/vitco/util/components/dialog/components/CheckBoxModule.java
+++ PS4k/src/com/vitco/util/components/dialog/components/CheckBoxModule.java
@@ -5,11 +5,7 @@ import com.vitco.util.components.dialog.BlankDialogModule;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.font.TextAttribute;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,15 +15,32 @@ import java.util.Map;
  */
 public class CheckBoxModule extends BlankDialogModule {
 
+    private static class JCustomCheckBox extends JCheckBox {
+        public JCustomCheckBox(String s) {
+            super(s);
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            if (this.isEnabled() || !this.isSelected()) {
+                super.paint(g);
+            } else {
+                this.setSelected(false);
+                super.paint(g);
+                this.setSelected(true);
+            }
+        }
+    }
+
     // the checkbox reference
-    private final JCheckBox checkbox;
+    private final JCustomCheckBox checkbox;
 
     // constructor
-    public CheckBoxModule(String identifier, String caption, boolean checked) {
+    public CheckBoxModule(final String identifier, String caption, final boolean checked) {
         super(identifier);
         setLayout(new BorderLayout());
         // add checkbox
-        checkbox = new JCheckBox(" " + caption); // add a bit more spacing
+        checkbox = new JCustomCheckBox(" " + caption); // add a bit more spacing
         checkbox.setFocusable(false);
         checkbox.setSelected(checked);
         // remove border (align to right side)
@@ -39,45 +52,6 @@ public class CheckBoxModule extends BlankDialogModule {
             @Override
             public void actionPerformed(ActionEvent e) {
                 notifyContentChanged();
-            }
-        });
-
-        // file "selected" property change event
-        checkbox.addItemListener(new ItemListener() {
-            private boolean oldValue = checkbox.isSelected();
-
-            public void itemStateChanged(ItemEvent e) {
-                checkbox.firePropertyChange("selected", oldValue, checkbox.isSelected());
-                oldValue = checkbox.isSelected();
-            }
-        });
-
-        // show as deselected when disabled
-        checkbox.addPropertyChangeListener(new PropertyChangeListener() {
-
-            private Boolean checkState = null;
-
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                if (evt.getPropertyName().equals("enabled")) { // if enabled state changed
-                    if (evt.getOldValue() != evt.getNewValue()) { // if it actually changes
-                        if ((Boolean)evt.getNewValue()) { // if checkbox is now enabled
-                            if (checkState != null) { // there is a known check state
-                                checkbox.setSelected(checkState); // restore check state
-                            }
-                        } else {
-                            checkState = checkbox.isSelected(); // remember check state
-                            checkbox.setSelected(false); // uncheck b/c disabled
-                        }
-                    }
-                }
-                if (!checkbox.isEnabled() && evt.getPropertyName().equals("selected")) { // disabled and the selected state changes
-                    if (evt.getOldValue() != evt.getNewValue()) { // if it actually changes
-                        // this might trigger more events (which is fine b/c we set the checkState second)
-                        checkbox.setSelected(false);  // uncheck b/c disabled
-                        checkState = (Boolean) evt.getNewValue();  // remember the state
-                    }
-                }
             }
         });
     }
@@ -98,7 +72,8 @@ public class CheckBoxModule extends BlankDialogModule {
     @Override
     protected ArrayList<String[]> getSerialization(String path) {
         ArrayList<String[]> keyValuePair = new ArrayList<String[]>();
-        keyValuePair.add(new String[] {path, getValue(null)});
+        // NOTE: we serialize the check state of the checkbox, so disabled checkboxes are serialized with the check state (!)
+        keyValuePair.add(new String[] {path, String.valueOf(checkbox.isSelected())});
         return keyValuePair;
     }
 
