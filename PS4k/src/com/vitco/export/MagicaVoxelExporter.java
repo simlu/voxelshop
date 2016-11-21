03c5026618d9ad23f95a18ea2befad96e2d406b1
blackflux <blackflux@unfine.de>
2016-11-20 20:21:38 -0800

    - improved exporter (now matching magica export)

--- PS4k/src/com/vitco/export/MagicaVoxelExporter.java
+++ PS4k/src/com/vitco/export/MagicaVoxelExporter.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 public class MagicaVoxelExporter extends AbstractExporter {
 
-    private final static int PALETTE_SIZE = 256;
+    private final static int PALETTE_SIZE = 255;
     private final static int MV_VERSION = 150;
     private final static int MV_DEFAULT_PALETTE_COLOR = new Color(75, 75, 75).getRGB();
 
@@ -26,10 +26,11 @@ public class MagicaVoxelExporter extends AbstractExporter {
 
         final int[] min = getMin();
         final int[] max = getMax();
+        final int[] size = getSize();
 
         final int[] colors = getColors();
         if (colors.length > PALETTE_SIZE) {
-            console.addLine("Error: MagicaVoxel *.vox format only supports 256 colors.");
+            console.addLine("Error: MagicaVoxel *.vox format only supports " + PALETTE_SIZE + " colors.");
             return false;
         }
         // prepare palette
@@ -45,40 +46,39 @@ public class MagicaVoxelExporter extends AbstractExporter {
 
         // MAIN Chunk
         fileOut.writeASCIIString("MAIN");
-        fileOut.writeIntRev(0); // Content Size
-        fileOut.writeIntRev((12 + 4 * 3) + (12 + 4 + 4 * getCount()) + (12 + 4 * PALETTE_SIZE));
+        fileOut.writeIntRev(0); // Main Chunk has no data, only children
+        // size of (1) "SIZE" chunk + (2) "XYZI" chunk + (3) "RGBA" chunk
+        fileOut.writeIntRev((12 + 4 * 3) + (12 + 4 + 4 * getCount()) + (12 + 4 * PALETTE_SIZE + 4));
 
         // Size Chunk
         fileOut.writeASCIIString("SIZE");
-        fileOut.writeIntRev(4 * 3);
-        fileOut.writeIntRev(0);
-        fileOut.writeIntRev(max[0] + min[0]);
-        fileOut.writeIntRev(max[2] + min[2]); // Maybe MV is Z-UP, Not sure but this just works
-        fileOut.writeIntRev(max[1] + min[1]);
-
+        fileOut.writeIntRev(4 * 3); // needed to store size
+        fileOut.writeIntRev(0); // no children
+        fileOut.writeIntRev(size[0]);
+        fileOut.writeIntRev(size[2]);
+        fileOut.writeIntRev(size[1]);
 
         // XYZI Chunk
         fileOut.writeASCIIString("XYZI");
-        fileOut.writeIntRev(4 + 4 * getCount());
-        fileOut.writeIntRev(0);
+        fileOut.writeIntRev(4 + 4 * getCount()); // needed to store size
+        fileOut.writeIntRev(0); // no children
         fileOut.writeIntRev(getCount());
 
         for (Voxel voxel : data.getVisibleLayerVoxel()) {
-            int colorId = colorPalette.get(voxel.getColor().getRGB()) + 1;
-
-            final int vx = max[0] - voxel.x;
-            final int vy = max[1] - voxel.y;
-            final int vz = max[2] - voxel.z;
+            final int vx = -(voxel.x - max[0]);
+            final int vy = -(voxel.y - max[1]);
+            final int vz = voxel.z - min[2];
+            final int colorId = colorPalette.get(voxel.getColor().getRGB()) + 1;
 
             fileOut.writeByte((byte)vx);
-            fileOut.writeByte((byte)vz); // Maybe MV is Z-UP, Not sure but this just works
+            fileOut.writeByte((byte)vz);
             fileOut.writeByte((byte)vy);
             fileOut.writeByte((byte)colorId);
         }
 
         // RGBA Chunk
         fileOut.writeASCIIString("RGBA");
-        fileOut.writeIntRev(4 * PALETTE_SIZE);
+        fileOut.writeIntRev(4 * PALETTE_SIZE + 4);
         fileOut.writeIntRev(0);
 
         for (int i = 0; i < PALETTE_SIZE; i++) {
@@ -88,6 +88,8 @@ public class MagicaVoxelExporter extends AbstractExporter {
             fileOut.writeByte((byte) (rgb & 0xFF));
             fileOut.writeByte((byte) 255);
         }
+        // last color is black
+        fileOut.writeIntRev(0xff000000);
         return true;
     }
 }
